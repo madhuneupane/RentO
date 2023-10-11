52a279250419976c2f19ed7eaa2d62f2bdbc8a3a
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-10-11 16:21:25 -0700

    created welcome screen

---
+++ frontend/src/components/container/WelcomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const WelcomeScreen = () => {
+    /*navigate this screen 
+     to Listings using ButtonUi component*/
+    
+     // To test-> change Filter to Welcome in Rentor
+  return (
+    <View>
+      <Text>Welcome</Text>    
+    </View>
+  )
+}
+
+export default WelcomeScreen

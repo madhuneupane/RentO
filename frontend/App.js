00f2edb0fdedd8fd4695bd5c78cde1d49dc1a2ee
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-10-11 22:15:21 -0700

    created Welcome Screen

--- frontend/App.js
+++ frontend/App.js
@@ -4,13 +4,14 @@ import AppStack from './src/components/stack/AppStack';
 import Filter from './src/components/container/Filter';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import LoginRentor from './src/components/container/LoginRentor';
+import WelcomeScreen from './src/components/container/WelcomeScreen';
 
 export default function App() {
   return (
   <SafeAreaProvider>
     <View style={styles.container}>
       <AppStack/>
-      {/* <LoginRentor/> */}
+      {/* <WelcomeScreen/> */}
     </View>
    </SafeAreaProvider>
 

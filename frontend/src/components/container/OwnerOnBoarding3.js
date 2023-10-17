d4c6a2cfe95ea1b5df447f6ba3f1ae153ba57a39
madhuneupane <nyoupanemadhu07@gmail.com>
2023-10-17 11:45:50 -0700

    feat: owner onboard 3 implement

--- frontend/src/components/container/OwnerOnBoarding3.js
+++ frontend/src/components/container/OwnerOnBoarding3.js
@@ -1,4 +1,61 @@
+import {View,StyleSheet,Text} from 'react-native'
+import {InputUI} from '../UI/input/InputUI'
+import ButtonUI from '../UI/button/ButtonUI'
+import { useState } from 'react'
 const OwnerOnboarding3 = ({navigation, route})=>{
-console.log(route.params);
+    const [onBoardData, setOnBoardData] = useState(
+       route.params
+        );
+    const navigateToNext = (value, type)=>{
+        setOnBoardData({...onBoardData,address:value})
+        //console.log(onBoardData);
+
+    }
+return(
+    <View>
+    <Text>What's the address?</Text>
+    <View>
+    <InputUI
+              placeholder=' Type your address'
+              selectedItems={navigateToNext}
+              type="address"
+              coustomStyle={styles}
+          />
+   
+    </View>
+</View>
+)
 }
 export default OwnerOnboarding3
+
+const styles = StyleSheet.create({
+    container: {
+          height: '70%',
+          justifyContent: 'center',
+          margin: 10,
+          padding:10   
+      },
+      subContainer: {
+          alignItems:'center'
+      },
+    textInput: {
+        fontSize: 15,
+        height: 30,
+        width: '100%',
+        borderRadius: 7,
+        textAlign:'auto'
+      },
+      button: {
+      borderWidth:1.5,
+      borderRadius: 15,
+      fontSize: 20,
+      textAlign: 'center',
+      marginTop: 30,
+      height:35
+      },
+      text: {
+          textAlign: 'center',
+          fontWeight: 'bold',
+          fontSize:'20'
+      }
+  })

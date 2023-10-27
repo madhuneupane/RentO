2c629c8e1db5846534465215492386861d29179b
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-10-27 16:58:44 -0700

    fix bugs

--- frontend/src/components/list/List.js
+++ frontend/src/components/list/List.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { FlatList,Text } from 'react-native'
-import ButtonUI from '../UI/button/ButtonUI'
-const List = ({items,selectedItems,numColumns,style,type}) => {
-    return (
-      <FlatList
-          data={items}
-            renderItem={({ item }) => (
-                <ButtonUI
-                    item={item}
-                    selectedItems={selectedItems}
-                    style={style}
-                    type={type}
-                />
-            )}
-            numColumns={numColumns}
-            />
-           
-  )
-}
+import React from "react";
+import { FlatList, Text } from "react-native";
+import ButtonUI from "../UI/button/ButtonUI";
+const List = ({ items, selectedItems, numColumns, style, type, check }) => {
+  return (
+    <FlatList
+      data={items}
+      renderItem={({ item }) => (
+        <ButtonUI
+          item={item}
+          selectedItems={selectedItems}
+          style={style}
+          type={type}
+          check={check}
+        />
+      )}
+      numColumns={numColumns}
+    />
+  );
+};
 
-export default List
-
+export default List;

104d721dc4e9d4b9cfd2d322a8e94204ff1fcdab
Laís Alves <109243775+lalves-hub@users.noreply.github.com>
2023-11-10 12:00:39 -0800

    card css

--- frontend/src/components/listItems/OwnerPostCards.js
+++ frontend/src/components/listItems/OwnerPostCards.js
@@ -10,13 +10,12 @@ const OwnerPostCards = ({ data, getTenant }) => {
         <View style={styles.viewContainer}>
           <Card.Image
             source={{ uri: data.images.bedrooms.back }}
-            style={{ width: "100%" }}
+            style={{ width: "90%" }}
           ></Card.Image>
         </View>
-        <View>
-          <Text>
-            {data.type} on {data.location}
-          </Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.type}>{data.type}</Text>
+          <Text style={styles.location}>{`on ${data.location}`}</Text>
         </View>
       </View>
     </TouchableOpacity>
@@ -25,18 +24,25 @@ const OwnerPostCards = ({ data, getTenant }) => {
 
 export default OwnerPostCards;
 const styles = StyleSheet.create({
+  textContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // Other container styles if needed
+  },
   container: {
     margin: 10,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
-    width: "90%",
+    width: "95%",
     border: "2",
     borderWidth: 1,
     borderRadius: 5,
     borderColor: "black",
     backgroundColor: "#E9E7EE",
     padding: 10,
+    fontSize: 0.5
     // height: 20,
     // borderRadius: 1,
     // padding: 10,
@@ -49,4 +55,19 @@ const styles = StyleSheet.create({
     // // width: "auto",
     width: "50%",
   },
+  textContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    // Other container styles if needed
+  },
+  type: {
+    textAlign: 'center',
+    width: "100%", // Adjust the width as needed
+    // Other text styles for data.type
+  },
+  location: {
+    textAlign: 'center',
+    width: "100%", // Adjust the width as needed
+    // Other text styles for data.location
+  }
 });

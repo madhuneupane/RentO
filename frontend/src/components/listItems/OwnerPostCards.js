cda3b1af6115da757d57cf87061079d4df26ff19
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-11-10 22:19:29 -0800

    Update OwnerPostCards.js

--- frontend/src/components/listItems/OwnerPostCards.js
+++ frontend/src/components/listItems/OwnerPostCards.js
@@ -22,14 +22,9 @@ const OwnerPostCards = ({ data, getTenant }) => {
     </TouchableOpacity>
   );
 };
+
 export default OwnerPostCards;
 const styles = StyleSheet.create({
-  textContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    // Other container styles if needed
-  },
   container: {
     margin: 10,
     marginLeft: 20,
@@ -43,9 +38,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderColor: "#36827F",
     backgroundColor: "#E9E7EE",
-    padding: 10,
-    fontSize: 0.5
-    // height: 20,
+    //padding: 10,
+    // height: "80%",
     // borderRadius: 1,
     // padding: 10,
     
@@ -63,23 +57,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     overflow:false,
   },
-  textContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    // Other container styles if needed
-  },
-  type: {
-    textAlign: 'center',
-    width: "100%", // Adjust the width as needed
-    // Other text styles for data.type
-  },
-  location: {
-    textAlign: 'center',
-    width: "100%", // Adjust the width as needed
-    // Other text styles for data.location
-  }
-});
-
   text:{
     color: "#36827F",
     fontFamily: "Mulish_700Bold",

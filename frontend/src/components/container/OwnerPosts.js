ab55cb93314cf61fff0436d5be62821c57401a4b
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-11-11 16:55:16 -0800

    css

--- frontend/src/components/container/OwnerPosts.js
+++ frontend/src/components/container/OwnerPosts.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import OwnerListingsList from "../list/OwnerListingsList";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ownerpost from "../hooks/ownerpost";
+import ButtonUI from "../UI/button/ButtonUI";
+
 const OwnerPosts = ({ navigation, route }) => {
   const [refreshing, setRefreshing] = useState(false);
 
@@ -14,16 +16,110 @@ const OwnerPosts = ({ navigation, route }) => {
   const [ownerPosts, setOwnerPosts] = useState();
   ownerpost(setOwnerPosts, refreshing);
   console.log("OwnerPosts::::" + ownerPosts);
+  var [isSubmitPress, setIsSubmitPress] = useState(false);
+
+  var touchPropsSubmit = {
+    underlayColor: "#B1D4D2",
+    style: isSubmitPress ? styles.submitButtonClicked : styles.submitButton,
+    onHideUnderlay: () => setIsSubmitPress(false),
+    onShowUnderlay: () => setIsSubmitPress(true),
+  };
   return (
-    <View>
+    <View style={styles.listContainer}>
       <OwnerListingsList
         ownerData={ownerPosts}
         setRefreshing={setRefreshing}
         refreshing={refreshing}
         refresh={refresh}
       />
+      <View style={styles.buttonContainer}>
+        <ButtonUI
+          item={{ value: "Manage Listings" }}
+          customStyle={styles.customStyle}
+          touchProps={touchPropsSubmit}
+        />
+      </View>
     </View>
   );
 };
 
 export default OwnerPosts;
+const styles = StyleSheet.create({
+  listContainer: {
+    flex: 0.5,
+  },
+  buttonContainer: {
+    // marginBottom: 20,
+    // backgroundColor: "pink",
+  },
+  customStyle: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  date: {
+    marginTop: 5,
+  },
+  container: {
+    margin: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "95%",
+    border: "2",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "black",
+    backgroundColor: "#E9E7EE",
+    padding: 10,
+    fontSize: 0.5,
+    // height: 20,
+    // borderRadius: 1,
+    // padding: 10,
+  },
+  viewContainer: {
+    // // margin: 0,
+    // flexDirection: "row",
+    // alignItems: "center",
+    // // justifyContent: "flex-start",
+    // // width: "auto",
+    width: "50%",
+  },
+  textContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    // Other container styles if needed
+  },
+  type: {
+    textAlign: "center",
+    width: "100%", // Adjust the width as needed
+    // Other text styles for data.type
+  },
+  location: {
+    textAlign: "center",
+    width: "100%", // Adjust the width as needed
+    // Other text styles for data.location
+  },
+  submitButton: {
+    backgroundColor: "#36827F",
+    borderColor: "#36827F",
+    height: "50",
+    width: "80%",
+    marginLeft: 40,
+    marginTop: 20,
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 40,
+  },
+  submitButtonClicked: {
+    backgroundColor: "#B1D4D2",
+    borderColor: "#B1D4D2",
+    height: "50",
+    width: "80%",
+    marginLeft: 40,
+    marginTop: 20,
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 40,
+  },
+});
